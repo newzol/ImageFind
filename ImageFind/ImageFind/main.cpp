4425e32c6e89942b01b5d6777134dde5a6626cc1
Dan Liu <danliu@dandembp.lan>
2017-03-22 15:15:11 +0800

    创建初始化项目

---
+++ ImageFind/ImageFind/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ImageFind
+//
+//  Created by Dan Liu on 2017/3/22.
+//  Copyright © 2017年 Dan Liu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
